a2bd965478fbeff0cd90b4663e7c707b58c56a47
Paulo-Senac <ph744915@gmail.com>
2024-05-28 10:32:35 -0300

    UC09

---
+++ Lista1/Lista1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pagamento, valor, troco;
+            int menu;
+
+
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - Média de Três Números");
+            Console.WriteLine("2 - Troco de Compra");
+            Console.WriteLine("3 - Conversão de Temperatura");
+            Console.Write("Escolha a opção: ");
+            menu = int.Parse(Console.ReadLine());
+
+
+            if (menu == 1)
+            {
+                float a, b, c;
+                float soma, media;
+
+                Console.Write("Digite o 1º numero: ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o 2º numero: ");
+                b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o 3º numero: ");
+                c = int.Parse(Console.ReadLine());
+
+                soma = a + b + c;
+                media = soma / 3;
+
+                Console.WriteLine("O valor da média é: " + media);
+
+            } else if (menu == 2)
+            {
+                Console.WriteLine("Digite o valor total da compra: ");
+                valor = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor pelo CLIENTE: ");
+                pagamento = float.Parse(Console.ReadLine());
+
+                if (valor > pagamento)
+                {
+                    Console.WriteLine("O valor do pagamento é insuficiente para concluir a compra");
+
+                }
+                else
+                {
+                    troco = pagamento - valor;
+                    Console.WriteLine("O valor do seu troco é: R$" + troco);
+
+                }
+
+
+            } else
+            {
+                float celsius, fahrenheit;
+
+                Console.WriteLine("Digite a temperatura em celsius: ");
+                celsius = float.Parse(Console.ReadLine());
+
+                fahrenheit = celsius * 9 / 5 + 32;
+                Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ola_Mundo/Ola_Mundo/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ola_Mundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string numero1 = txbNumero1.Text;
+            string numero2 = txbNumero2.Text;
+
+            int total = int.Parse(numero1) + int.Parse(numero2);
+
+            lbResultado.Text =  total.ToString();
+        }
+
+        private void lbResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+
+            string numero1 = txbNumero1.Text;
+            string numero2 = txbNumero2.Text;
+
+            int total = int.Parse(numero1) - int.Parse(numero2);
+
+            lbResultado.Text = total.ToString();
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            string numero1 = txbNumero1.Text;
+            string numero2 = txbNumero2.Text;
+
+            int total = int.Parse(numero1) * int.Parse(numero2);
+
+            lbResultado.Text = total.ToString();
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            string numero1 = txbNumero1.Text;
+            string numero2 = txbNumero2.Text;
+
+            float total = float.Parse(numero1) / float.Parse(numero2);
+
+            lbResultado.Text = total.ToString();
+        }
+
+        private void btnMaiorMenor_Click(object sender, EventArgs e)
+        {
+            int numero3 = int.Parse(txbNumero3.Text);
+
+            if ( numero3 < 0 )
+            {
+                lbResultado.Text = "O numero digitado é MENOR que 0";
+            } else
+            {
+                lbResultado.Text = "O numero digitado é MAIOR que 0";
+            }
+
+        }
+    }
+}
