23dea4ef3e8e45abe608a7377aaa1cb942c8f7b3
Paulo-Senac <ph744915@gmail.com>
2024-05-28 10:31:38 -0300

    UC12

---
+++ arquivoEmTexto/arquivoEmTexto/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arquivoEmTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string LerArquivo(string endereco)
+        {
+            string conteudo = "";
+
+            if (File.Exists(endereco))
+            {
+                using (StreamReader sr = new StreamReader(endereco))
+                {
+                    conteudo = sr.ReadToEnd();
+                }
+            }
+            return conteudo;
+        }
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string nome = txbInput.Text;
+            string telefone = txbTelefone.Text;
+            string email = txbEmail.Text;
+            string separador = "|";
+            
+            if (rbTelefone.Checked)
+            {
+                email = "-";
+            } else if (rbEmail.Checked)
+            {
+                telefone = "-";
+            }
+
+            string contato = nome + separador + telefone + separador + email;
+
+            string caminho = "C:/Users/paulo.hsantos26/source/repos/aula-algoritmo-TI21/UC12 - CSharp/arquivoEmTexto/dados.txt";
+            string textoAntigo = LerArquivo(caminho);
+
+            using (StreamWriter sw = new StreamWriter(caminho))
+            { 
+                sw.WriteLine(textoAntigo +  contato);
+            }
+
+            MessageBox.Show("Arquivo criado com sucesso!");
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string caminho = "C:/Users/paulo.hsantos26/source/repos/aula-algoritmo-TI21/UC12 - CSharp/arquivoEmTexto/dados.txt";
+
+            string texto = LerArquivo(caminho);
+
+            Array lista = texto.Split('\n');
+
+            foreach (string pessoa in lista)
+            {
+                string[] dados = pessoa.Split('|');
+
+                //MessageBox.Show(dados[0]);
+
+                cbxNome.Items.Add(dados[0].ToUpper());
+            }
+        }
+    }
+}
---
+++ quiz_p/quiz_p/FrmCadastrarPergunta.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace quiz_p
+{
+    public partial class frmCadastrarPergunta : Form
+    {
+        public frmCadastrarPergunta()
+        {
+            InitializeComponent();
+        }
+
+        string conexaoString =
+            "server=localhost;user=root;password=;database=db_quiz_p;";
+        public long CadastrarAlternativas(string alternativa, long perguntaID)
+        {
+            long idAlternativa = 0;
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                conexao.Open();
+
+                string insert_tb_alternativa = "INSERT INTO tb_alternativa (respostas, id_pergunta) VALUE (@respostas, @id_pergunta)";
+
+                using (MySqlCommand comando = new MySqlCommand(insert_tb_alternativa, conexao))
+                {
+                    comando.Parameters.AddWithValue("@respostas", alternativa);
+                    comando.Parameters.AddWithValue("@id_pergunta", perguntaID);
+
+                    comando.ExecuteNonQuery();
+                    idAlternativa = comando.LastInsertedId;
+                }
+            }
+
+            return idAlternativa;
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string pergunta = "";
+            string alternativaA = "", alternativaB = "",
+                alternativaC = "", alternativaD = "";
+            long ultimoID = 0;
+            long idAlternativaA = 0, idAlternativaB = 0, idAlternativaC = 0, idAlternativaD = 0;
+
+            pergunta = rtxPergunta.Text;
+
+            alternativaA = txbAlternativaA.Text;
+            alternativaB = txbAlternativaB.Text;
+            alternativaC = txbAlternativaC.Text;
+            alternativaD = txbAlternativaD.Text;
+
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                conexao.Open();
+                string scriptInsert = "INSERT INTO tb_perguntas (questao) VALUE (@questao)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsert, conexao))
+                {
+                    //substitui os parametros para os valores reais
+                    comando.Parameters.AddWithValue("@questao", pergunta);
+
+                    comando.ExecuteNonQuery();
+
+                    ultimoID = comando.LastInsertedId;
+                }
+            }
+
+            idAlternativaA = CadastrarAlternativas(alternativaA, ultimoID);
+            idAlternativaB = CadastrarAlternativas(alternativaB, ultimoID);
+            idAlternativaC = CadastrarAlternativas(alternativaC, ultimoID);
+            idAlternativaD = CadastrarAlternativas(alternativaD, ultimoID);
+
+            if (rbAlternativaCorretaA.Checked)
+            {
+                CadastrarResposta(ultimoID, idAlternativaA);
+            }
+            else if (rbAlternativaCorretaB.Checked)
+            {
+                CadastrarResposta(ultimoID, idAlternativaB);
+            }
+            else if (rbAlternativaCorretaC.Checked)
+            {
+                CadastrarResposta(ultimoID, idAlternativaC);
+            }
+            else if (rbAlternativaCorretaD.Checked)
+            {
+                CadastrarResposta(ultimoID, idAlternativaD);
+            }
+
+            MessageBox.Show("Pergunta Cadastrada com Sucesso!!! " + ultimoID.ToString());
+        }
+
+        public void CadastrarResposta(long idPergunta, long idAlternativa)
+        {
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                conexao.Open();
+                string scriptInsertResposta = "INSERT INTO tb_resposta (id_altenativa, id_pergunta) VALUE (@id_alternativa, @id_pergunta)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsertResposta, conexao))
+                {
+                    comando.Parameters.AddWithValue("@id_pergunta", idPergunta);
+                    comando.Parameters.AddWithValue("@id_alternativa", idAlternativa);
+
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            Form consultarPergunta = new FrmConsultarPergunta();
+
+            consultarPergunta.Show();
+
+        }
+    }
+}
