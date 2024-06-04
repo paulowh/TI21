3eb201ab0011dac3f545db9c9b1cc882564e9738
Paulo-Senac <ph744915@gmail.com>
2024-06-04 10:53:50 -0300

    Loop PHP em cards

---
+++ loop/pages/produto.php
@@ -0,0 +1,26 @@
+<section id="produtos">
+	<h2 class="titulo">Produtos</h2>
+	<main class="container">
+		<div class="row">
+			<?php foreach ($produtos as $key => $info) { ?>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<figure>
+						<!-- img alterar -->
+						<img src="./img/<?= $info['poster'] ?>" alt="poster do jogo Jedi Survivor" class="foto-produto">
+						<figcaption>
+							<h4><?= $info['titulo'] ?></h4> <!-- alterar -->
+							<span class="preco">R$<?= $info['preco'] ?>,00</span> <!-- alterar -->
+							<span class="avaliacao">
+								<i class="bi bi-star-fill"></i>
+								<i class="bi bi-star-fill"></i>
+								<i class="bi bi-star-fill"></i>
+								<i class="bi bi-star-half"></i>
+								<i class="bi bi-star"></i>
+							</span>
+						</figcaption>
+					</figure>
+				</div>
+			<?php } ?>
+		</div>
+	</main>
+</section>
